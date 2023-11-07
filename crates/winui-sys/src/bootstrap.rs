287eecb2aca190e2c7c59afd0ba392fa05b095ac
Luca Lewin <luca.lewin23@gmail.com>
2023-11-07 22:08:06 +0100

    show popup when appsdk is not installed

--- crates/winui-sys/src/bootstrap.rs
+++ crates/winui-sys/src/bootstrap.rs
@@ -12,9 +12,10 @@ const RELEASE_MAJOR_MINOR: u32 = 0x00010004;
 const VERSION_TAG: ffi::PCWSTR = "".as_ptr() as *const u16;
 const RUNTIME_VERSION: u64 = 0x0FA003DA02630000;
 const PACKAGE_VERSION: ffi::PACKAGE_VERSION = ffi::PACKAGE_VERSION {__bindgen_anon_1: ffi::PACKAGE_VERSION__bindgen_ty_1 { Version: RUNTIME_VERSION }};
+const BOOTSTRAP_OPTIONS: ffi::MddBootstrapInitializeOptions = ffi::MddBootstrapInitializeOptions_MddBootstrapInitializeOptions_OnNoMatch_ShowUI;
 
 pub fn initialize() -> std::io::Result<()> {
-    unsafe { ffi::MddBootstrapInitialize(RELEASE_MAJOR_MINOR, VERSION_TAG, PACKAGE_VERSION) };
+    unsafe { ffi::MddBootstrapInitialize2(RELEASE_MAJOR_MINOR, VERSION_TAG, PACKAGE_VERSION, BOOTSTRAP_OPTIONS) };
 
     Ok(())
 }
